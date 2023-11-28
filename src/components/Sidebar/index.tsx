dcf9eaf64146570081766af4dbbdc127d2e244af
FelipeBaranda <felbaranda@gmail.com>
2023-11-28 19:36:26 -0300

    build assets

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,25 @@
+import { Container } from './styles'
+
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+
+import menuImg from '../../assets/menu.svg'
+
 export function Sidebar() {
-  return <h1>Sidebar</h1>
+  return (
+    <Container>
+      <button type='button'>
+        <img src={menuImg} alt='Abrir e fechar o menu' />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href='#'>
+              <BurgerIcon />
+              <span>Hambúrgueres</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
 }
