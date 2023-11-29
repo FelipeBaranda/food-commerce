a3c018a0cde3719faa6f1467938961f66efa8d30
FelipeBaranda <felbaranda@gmail.com>
2023-11-28 21:30:37 -0300

    formatando os snacks

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,3 +1,5 @@
+import { FiPlus } from 'react-icons/fi'
+
 import { Container } from './styles'
 
 interface SnacksProps {
@@ -14,7 +16,9 @@ export function Snacks({ snacks }: SnacksProps) {
           <p>{snack.description}</p>
           <div>
             <strong>{snack.price}</strong>
-            <button type='button'></button>
+            <button type='button'>
+              <FiPlus />
+            </button>
           </div>
         </div>
       ))}
